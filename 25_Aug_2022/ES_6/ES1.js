5a64c96cf5c4ee6694b6c7ab42564952c33fc5e7
Mohd Ziya <iamziya53@gmail.com>
2022-09-05 17:30:42 +0530

    ES_6

---
+++ 25_Aug_2022/ES_6/ES1.js
@@ -0,0 +1,10 @@
+// function talk(msg){
+//     console.log(msg);
+// }
+// // talk("Hellow")   output:Hellow
+// //  talk();         output:  undefined
+
+function talk(msg="Ziya"){
+    console.log(msg)
+}
+talk()
