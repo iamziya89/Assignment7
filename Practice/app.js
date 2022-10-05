3b0d8f1f6768a78ad187c12e44531f6ea990956b
Mohd Ziya <iamziya53@gmail.com>
2022-10-05 13:04:40 +0530

    ES_06

---
+++ Practice/app.js
@@ -0,0 +1,100 @@
+// let list_2=document.getElementById("id1")
+// console.log(list_2);
+// list_2.style.color="red";
+
+// let elm=document.getElementsByTagName('li');
+// for(let i=0;i<elm.length;i++){
+//     // console.log(elm[i]);
+//     elm[i].innerHTML="Mohammad Ziya"
+// }
+
+// function talk(msg){
+//     console.log(msg);
+// }
+// talk("ziya");
+// talk();
+
+// function talk(msg="Ziya"){
+//     console.log(msg);
+// }
+// talk();
+
+
+// function add(a,b){
+//     console.log(a+b);
+// }
+// add(15,25,15);
+
+// function sum(...args){
+//     console.log(args);
+// }
+// sum(2,5,7,8,9);
+
+// function sum(...args){
+//     let result=0;
+//     for(let i=0;i<args.length;i++){
+//         result+=args[i];
+//     }
+//     console.log(result);
+// }
+// sum(2,4,3,1,5)
+
+
+// let arr1=[4,5,6]
+// let arr2=[...arr1,1,2,3,]
+// console.log(arr2);
+
+// let arr=[11,2,13,41,5]
+// for(let i of arr){
+//     console.log(i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
